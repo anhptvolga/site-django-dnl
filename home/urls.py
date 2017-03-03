e05c1f8b08d0ca188a7ddb651d62283a9e5aa687
godric <mr.pt.anh.bsb@gmail.com>
2017-03-03 17:20:03 +0300

    add urls

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 
 urlpatterns = [
+    url(r'^$', views.home, name='home'),
+    url(r'^news/$', views.news, name='news'),
+    url(r'^contact/$', views.contact, name='contact'),
+    url(r'^comps/', views.compositions, name='comps'),
 ]
