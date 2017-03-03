ac5db9e2c852970c64e3feb90ba7f6f298611d89
godric <mr.pt.anh.bsb@gmail.com>
2017-03-03 17:19:36 +0300

    create views

--- home/views.py
+++ home/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
-
+from .models import Contact
 # Create your views here.
+
+
+def home(request):
+    """
+        View of  page
+    """
+    return render(request, "home.html", {
+        'home_active': True,
+    })
+
+
+def news(request):
+    """
+        View of news page
+    """
+    return render(request, "news.html", {
+        'news_active': True,
+    })
+
+
+def compositions(request):
+    """
+        View of compositions page
+    """
+    return render(request, "comps.html", {
+        'comps_active': True,
+    })
+
+
+def contact(request):
+    """
+        View of contact page
+    """
+    contact = Contact.objects.get(pk=1)
+    return render(request, "contact.html", {
+        'contact_active': True,
+        'address': contact.address,
+        'email': contact.email,
+        'phone': contact.phone
+    })
+
