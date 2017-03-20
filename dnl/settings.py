3fc67279e71a29152b3953fba97073d2ee697038
godric <mr.pt.anh.bsb@gmail.com>
2017-03-20 14:17:12 +0300

    turn on messages framework

--- dnl/settings.py
+++ dnl/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -133,3 +134,11 @@ STATIC_ROOT = [
 
 MEDIA_ROOT = BASE_DIR
 MEDIA_URL = '/media/'
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
